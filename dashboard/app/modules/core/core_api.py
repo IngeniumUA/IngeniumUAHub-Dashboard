fe92cae5e3576ac85c7d74017e28f813c834a15d
Robbe De Helt <11708254+DrSqua@users.noreply.github.com>
2025-03-28 23:01:30 +0100

    Working on authentication .. slow progress

--- dashboard/app/modules/core/core_api.py
+++ dashboard/app/modules/core/core_api.py
@@ -1,5 +1,18 @@
-import requests
+from typing import Optional
 
+from keycloak import KeycloakOpenID
 
-def fetch_user_table():
-    return requests.get("https://dev.ingeniumua.be/api/v1/user")
+from app.settings import settings
+
+class CoreClient:
+    def __init__(self, username: str, password: str):
+        self.keycloak_token: Optional[dict] = None
+
+        self.username = username
+        self.password = password
+
+    def _keycloak_access_token(self):
+        keycloak_openid = KeycloakOpenID(server_url=settings.keycloak_server_url,
+                                         client_id="backend-api",
+                                         realm_name='master')
+        self.keycloak_token = keycloak_openid.token(self.username, self.password)
--- dashboard/app/page/main_page.py
+++ dashboard/app/page/main_page.py
@@ -1,5 +1,4 @@
 import streamlit as st
-
 from app.settings import settings
 
 
@@ -7,6 +6,22 @@ def main_page():
     st.title("Main Page")
     if "keycloak_token" in st.session_state:
         st.write(st.session_state["keycloak_token"])
+    else:
+        st.write("No keycloak token stored")
 
     # Showing config
     st.write(settings.model_dump())
+
+    # Core API
+    # core_client = CoreClient("dev_backendapiaccess", "")
+    # core_client._keycloak_access_token()
+    #
+    # headers = {'authorization': f"Bearer {core_client.keycloak_token["access_token"]}"}
+    # params = {}
+    # with httpx.Client(headers=headers, params=params, base_url=settings.core_api_url) as client:
+    #     response = client.get(url="user", headers=headers)
+    #     st.write(response.status_code)
+    #     st.write(response.text)
+    #     st.write(response.content)
+
+
