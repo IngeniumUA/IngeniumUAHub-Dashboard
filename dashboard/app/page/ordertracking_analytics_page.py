45be71881415bb10dbc29b800228bc1e0749bc6a
DrSqua <11708254+DrSqua@users.noreply.github.com>
2025-05-24 13:59:19 +0000

    Automated style fixes by ruff

--- dashboard/app/page/ordertracking_analytics_page.py
+++ dashboard/app/page/ordertracking_analytics_page.py
@@ -1,5 +1,6 @@
 import streamlit as st
 
+
 @st.fragment
 def order_tracking_analytics_page():
     # -----
@@ -15,18 +16,26 @@ def order_tracking_analytics_page():
         ingestion_col, _, _ = st.columns(3)
 
         ingestion_col.text("Ingestion")
-        ingestion_col.toggle(label="Checkout Data",
-                  value=st.session_state.get("checkout_data_ingestion"), 
-                  key="checkout_data_ingestion")
-        ingestion_col.toggle(label="Transaction Data",
-                  value=st.session_state.get("transaction_data_ingestion"),
-                  key="transaction_data_ingestion")
-        ingestion_col.toggle(label="Checkout Tracker Data",
-                  value=st.session_state.get("checkout_tracker_data_ingestion"),
-                  key="checkout_tracker_data_ingestion")
-    ingestion_col.toggle(label="Log data",
-                         value=st.session_state.get("log_data_ingestion"),
-                         key="log_data_ingestion")
+        ingestion_col.toggle(
+            label="Checkout Data",
+            value=st.session_state.get("checkout_data_ingestion"),
+            key="checkout_data_ingestion",
+        )
+        ingestion_col.toggle(
+            label="Transaction Data",
+            value=st.session_state.get("transaction_data_ingestion"),
+            key="transaction_data_ingestion",
+        )
+        ingestion_col.toggle(
+            label="Checkout Tracker Data",
+            value=st.session_state.get("checkout_tracker_data_ingestion"),
+            key="checkout_tracker_data_ingestion",
+        )
+    ingestion_col.toggle(
+        label="Log data",
+        value=st.session_state.get("log_data_ingestion"),
+        key="log_data_ingestion",
+    )
 
     with st.container(border=True):
         st.subheader("Summary Analytics")
--- dashboard/app/page/transaction_analytics_page.py
+++ dashboard/app/page/transaction_analytics_page.py
@@ -5,49 +5,56 @@ import polars as pl
 from plotly.subplots import make_subplots
 import plotly.graph_objects as pgo
 
-def transaction_analytics(container: DeltaGenerator,
-                          dataframe: pl.DataFrame,
-                          key: str):
+
+def transaction_analytics(container: DeltaGenerator, dataframe: pl.DataFrame, key: str):
     if dataframe.is_empty():
         container.warning("Input dataframe is empty")
         return
 
     time_interval_key = key + "_time_interval_key"
-    options = ('5m', '30m', '1h', '1d'),
+    options = (("5m", "30m", "1h", "1d"),)
     time_interval = container.selectbox(
         label="Select time interval",
         options=options,
         key=time_interval_key,
-        index=options.index(st.session_state.get(time_interval_key, options[len(options) // 2])) # Default to middle
+        index=options.index(
+            st.session_state.get(time_interval_key, options[len(options) // 2])
+        ),  # Default to middle
     )
 
     #
-    per_timestamp = dataframe.group_by(
-        pl.col('created_timestamp').dt.round(time_interval)
-    ).agg(
-        pl.col('id').count().alias('transaction_count'),
-    ).sort(
-        'created_timestamp'
+    per_timestamp = (
+        dataframe.group_by(pl.col("created_timestamp").dt.round(time_interval))
+        .agg(
+            pl.col("id").count().alias("transaction_count"),
+        )
+        .sort("created_timestamp")
     )
     #
-    per_product_blueprint = dataframe.group_by(
-        pl.col('blueprint_name')
-    ).agg(
-        pl.col('id').count().alias('transaction_count'),
-    ).sort(
-        'transaction_count'
+    per_product_blueprint = (
+        dataframe.group_by(pl.col("blueprint_name"))
+        .agg(
+            pl.col("id").count().alias("transaction_count"),
+        )
+        .sort("transaction_count")
     )
 
-    fig = make_subplots(1, 2,
-                        x_title=f"Bestellingen per {time_interval}, alle dagen",
-                        subplot_titles=["Orders (n) as f of t"])
+    fig = make_subplots(
+        1,
+        2,
+        x_title=f"Bestellingen per {time_interval}, alle dagen",
+        subplot_titles=["Orders (n) as f of t"],
+    )
     fig.add_trace(
-        pgo.Scatter(x=per_timestamp["created_timestamp"], y=per_timestamp['transaction_count'],
-                    mode='lines+markers',
-                    name=f"Orders per {time_interval}",
-                    fill="tozeroy"
-                    ),
-        row=1, col=1
+        pgo.Scatter(
+            x=per_timestamp["created_timestamp"],
+            y=per_timestamp["transaction_count"],
+            mode="lines+markers",
+            name=f"Orders per {time_interval}",
+            fill="tozeroy",
+        ),
+        row=1,
+        col=1,
     )
     fig.add_trace(
         pgo.Bar(
@@ -55,9 +62,10 @@ def transaction_analytics(container: DeltaGenerator,
             x=per_product_blueprint["blueprint_name"],
             y=per_product_blueprint["transaction_count"],
             text=per_product_blueprint["transaction_count"],  # Display values
-            textposition="auto"  # Automatically position the text
+            textposition="auto",  # Automatically position the text
         ),
-        row=1, col=2
+        row=1,
+        col=2,
     )
     container.plotly_chart(fig)
 
