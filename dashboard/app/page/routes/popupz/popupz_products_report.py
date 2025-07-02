70f2e69ff5834c1d8ce4a83aaea5fb7f1896c4e6
Robbe De Helt <11708254+DrSqua@users.noreply.github.com>
2025-07-02 13:15:03 +0200

    Brainstorm for popupz reports page

---
+++ dashboard/app/page/routes/popupz/popupz_products_report.py
@@ -0,0 +1,10 @@
+def popupz_orders_report_page():
+    """
+
+    Ideas:
+
+
+    todo for each product on timeseries, calculate per time bucket the percentage of orders (for that product) in that bucket on total
+
+    todo checkouttrackers split on created and last edited timestamp so we can calculate (and plot) wait times
+    """
