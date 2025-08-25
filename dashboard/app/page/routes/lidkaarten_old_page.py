9f3a457c89644979e3f7f06af901354f30161779
Robbe De Helt <11708254+DrSqua@users.noreply.github.com>
2025-08-25 21:06:14 +0200

    Adding download button

--- dashboard/app/page/routes/lidkaarten_old_page.py
+++ dashboard/app/page/routes/lidkaarten_old_page.py
@@ -8,17 +8,17 @@ def lidkaarten_overview_old_page():
     st.title('Lidkaarten')
 
     core_client = get_core_client()
-    gathered_response = []
-    with st.status("Loading .."):
-        for i in range(0, 700//50):
-            st.write(i)
-            try:
-                response = core_client.client.get(f"api/v1/item/wide/card?limit=50&offset={i*50}")
-                response_list = TypeAdapter(list[dict]).validate_python(response.json())
-                gathered_response.extend(response_list)
-            except Exception as e:
-                st.error(e)
-                return
-    source_df = pl.json_normalize(gathered_response, max_level=1)
+    response = core_client.client.get(f"api/v1/item/wide/card?limit=700")
+    response_list = TypeAdapter(list[dict]).validate_python(response.json())
+    source_df = pl.json_normalize(response_list, max_level=1)
+
+    csv_bytes = source_df.write_csv().encode("utf-8")
+    st.download_button(
+        label="Download as CSV",
+        data=csv_bytes,
+        file_name="lidkaarten.csv",
+        mime="text/csv",
+    )
+
     st.dataframe(source_df)
 
