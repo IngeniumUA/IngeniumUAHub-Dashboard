61a47231a3ea972cb75678002ec8867d7fd08d8c
Robbe De Helt <11708254+DrSqua@users.noreply.github.com>
2025-08-25 20:41:17 +0200

    Production still has max set to 50 to doing a quick workaround there

--- dashboard/app/page/routes/lidkaarten_old_page.py
+++ dashboard/app/page/routes/lidkaarten_old_page.py
@@ -1,5 +1,6 @@
 import streamlit as st
 import polars as pl
+from pydantic import TypeAdapter
 
 from app.page.cached_resources.clients import get_core_client
 
@@ -7,7 +8,17 @@ def lidkaarten_overview_old_page():
     st.title('Lidkaarten')
 
     core_client = get_core_client()
-    response = core_client.client.get("api/v1/item/wide/card?limit=1000")
-    source_df = pl.json_normalize(response, max_level=1)
+    gathered_response = []
+    with st.progress():
+        for i in range(0, 700//50):
+            st.write(i)
+            try:
+                response = core_client.client.get(f"api/v1/item/wide/card?limit=50&offset={i*50}")
+                response_list = TypeAdapter(list[dict]).validate_json(response.json())
+                gathered_response.extend(response_list)
+            except Exception as e:
+                st.error(e)
+                return
+    source_df = pl.json_normalize(gathered_response, max_level=1)
     st.dataframe(source_df)
 
